b6bee65dd8d20609eb2e011ca05d85bce6dc3d66
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2020-04-01 00:00:47 +0530

    Create 3_4_8.kt

---
+++ 3_4_8.kt
@@ -0,0 +1,25 @@
+fun main() {
+println("********Using for Loop*******")
+for(i in  100 downTo 1){
+    if(i%2!=0)
+        println(i)
+}
+println("********Using while Loop*******")
+var j=100
+while(j>=0){
+    if(j%2!=0)
+        println(j)
+    j--
+}
+println("********Using  do while Loop*******")
+var k=100
+do{
+    if(k%2!=0)
+        println(k)
+    k--
+}
+while(k>=0)
+    
+    // write your code here
+    
+}
