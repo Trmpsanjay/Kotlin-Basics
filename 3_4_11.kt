e64b2955dc18a91c9813d3deadf3f55d279636c5
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2020-04-01 00:24:09 +0530

    Create 3_4_11.kt

---
+++ 3_4_11.kt
@@ -0,0 +1,13 @@
+fun main() {
+    var a=mutableListOf( "Lionel Messi", "Wayne Rooney", "Luka Modric", "Eden Hazard", "Sergio Aguero")
+    a.add("Harry Kane")
+    a.remove("Wayne Rooney")
+     a.add(1,"Nemar")
+    println(a)
+     var b=listOf( "Lionel Messi", "Wayne Rooney", "Luka Modric", "Eden Hazard", "Sergio Aguero")
+    println(b)
+     var c=arrayListOf( "Lionel Messi", "Wayne Rooney", "Luka Modric", "Eden Hazard", "Sergio Aguero")
+    println(c)
+    // write your code here
+    
+}
