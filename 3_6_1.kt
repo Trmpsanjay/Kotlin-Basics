00bc51a2ae88a9fd171226b5713d275faa3d880e
SANJAY KUMAR <44132320+Trmpsanjay@users.noreply.github.com>
2020-04-01 22:36:33 +0530

    Create 3_6_1.kt

---
+++ 3_6_1.kt
@@ -0,0 +1,24 @@
+// create your Laptop class here
+class Laptop{
+    var RAM="4GB"
+    var brand="HP"
+    var hdd="1TB"
+    fun performComputations(){
+    println(RAM)
+    println(brand)
+    println(HDD)
+        
+    }
+}
+
+
+fun main(){
+
+    // Create object of class here    
+    
+    println(laptop.ram)
+    println(laptop.brand)
+    println(laptop.hdd)
+    laptop.performComputations()
+   
+}
